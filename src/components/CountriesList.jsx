695f5427aead3c16bfcf7c29b3adaf9110c808af
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-23 19:30:29 +0100

    Iteration 2 is completed with Istvan.

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 function CountriesList({ countries }) {
-  return (
-    <div>
-      {countries.map((country) => (
-        <div key={country.name.official} className="country">
-          <Link to={`/${country.alpha3Code}`}>
-          {country.alpha3Code}
-          
-          </Link>
+    return (
+        <div>
+            {countries.map((country) => (
+                <div key={country.name.official} className="country">
+                    <div>
+                        <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.common}></img>
+                    </div>
+                    <Link to={`/${country.alpha3Code}`}>
+                        {country.alpha3Code}
+
+                    </Link>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default CountriesList;
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -15,6 +15,10 @@ function CountryDetails({ countries }) {
    
   
       <div>
+        <img
+        src={`https://flagpedia.net/data/flags/icon/120x90/${foundCountry.alpha2Code.toLowerCase()}.png`}
+        alt="countryflag"
+      />
           <h2>{foundCountry.name.official}</h2>
           <table class="table">
             <tbody>
@@ -26,7 +30,20 @@ function CountryDetails({ countries }) {
                 <th scope="row">Area</th>
                 <td>{foundCountry.area} km²</td>
               </tr>
-              
+              <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {foundCountry.borders.map((current) => {
+                  return (
+                    <div>
+                      <Link to={`/country/${current}`}>{current}</Link>
+                    </div>
+                  );
+                })}
+              </ul>
+            </td>
+          </tr>
             </tbody>
           </table>
         </div>
