00d7cc99695b963a5970175565c2e5ae1c5e2c82
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-23 15:22:30 +0100

    Iteration 1 is completed w/ Istvan

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Router>
 )
+
